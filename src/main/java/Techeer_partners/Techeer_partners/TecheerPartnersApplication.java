daecfd727c25dc520c54af87419c7159bdc4e977
woohyun23 <parkwh0823@gmail.com>
2024-09-23 16:00:55 +0900

    Initial commit

---
+++ src/main/java/Techeer_partners/Techeer_partners/TecheerPartnersApplication.java
@@ -0,0 +1,13 @@
+package Techeer_partners.Techeer_partners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerPartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheerPartnersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Techeer_partners/Techeer_partners/TecheerPartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package Techeer_partners.Techeer_partners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerPartnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
