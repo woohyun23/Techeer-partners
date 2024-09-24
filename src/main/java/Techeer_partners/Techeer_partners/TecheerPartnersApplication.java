f5c85ef4cd5be0ff3070a70ae6a86d3a90cf665e
woohyun23 <parkwh0823@gmail.com>
2024-09-24 18:28:00 +0900

    김영한 스프링 입문 ~6강

--- src/main/java/Techeer_partners/Techeer_partners/TecheerPartnersApplication.java
+++ src/main/java/Techeer_partners/Techeer_partners/TecheerPartnersApplication.java
@@ -8,13 +8,6 @@ import java.util.Scanner;
 public class TecheerPartnersApplication {
 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int num = sc.nextInt();
-		for(int i = 0; i < num; i++){
-			System.out.println(i);
-		}
 		SpringApplication.run(TecheerPartnersApplication.class, args);
 
 	}
